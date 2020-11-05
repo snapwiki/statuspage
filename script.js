54fad8b860ba43deef2a865d48c1552ee62c971f
R4356th <61620631+R4356th@users.noreply.github.com>
2020-11-05 16:19:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Everything is fine on the wiki right now!")
+document.addEventListener('DOMContentLoaded', fetcher());
+function fetcher() {
+    fetch('https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/users/R4356th/messages/count')
+    .then(response => response.json())
+    .then(data => {
+        const count = data.status;
+        console.log(status); 
+        document.getElementById("status").innerHTML = `${status}`;
+    })
+    .catch(error => {
+        console.error("An error occured while fetching the message count:", error);
+    })
+};
